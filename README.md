714dad2e2bc618248ed240cc5224395319d2f26d
André de Alencar Porto <andre.porto@serpro.gov.br>
2020-04-28 17:40:57 -0300

    versão pós refactoring final do módulo 3 de android essential - Alura

--- README.md
+++
@@ -1,2 +0,0 @@
-# android-pseudo-agenda
-Projeto de estudo de conceitos de desenvolvimento Android nativo - parte 1
---
+++ app/src/main/java/br/com/alura/agenda/AgendaApplication.java
@@ -0,0 +1,22 @@
+package br.com.alura.agenda;
+
+import android.app.Application;
+
+import br.com.alura.agenda.dao.AlunoDAO;
+import br.com.alura.agenda.model.Aluno;
+
+@SuppressWarnings("WeakerAccess")
+public class AgendaApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        criaAlunosDeTeste();
+    }
+
+    private void criaAlunosDeTeste() {
+        AlunoDAO dao = new AlunoDAO();
+        dao.salva(new Aluno("Fulano de Tal", "81999998888", "fulano@tal.com.br"));
+        dao.salva(new Aluno("Beltrano da Tal", "81988887777", "beltrano@tal.com"));
+    }
+}
